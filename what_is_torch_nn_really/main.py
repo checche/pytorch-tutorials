50df16e99d9509eac0ddaf137f3b48de6bbfacde
Takahiro Hirata <ta.hirata@opt.ne.jp>
2020-12-18 18:36:19 +0900

    What is torch.nn really?: MNIST data setup

---
+++ what_is_torch_nn_really/main.py
@@ -0,0 +1,26 @@
+# %%
+from IPython.display import display
+
+import dataset
+import models
+
+
+# Load the "autoreload" extension so that code can change
+%load_ext autoreload
+
+# always reload modules so that as you change code in src, it gets loaded
+%autoreload 2
+
+# %% [markdown]
+# ## MNIST data setup
+# %%
+x_train, y_train, x_valid, y_valid = dataset.make_dataset()
+# %%
+n, c = x_train.shape
+display(
+    x_train,
+    y_train,
+    x_train.shape,
+    y_train.min(),
+    y_train.max()
+)
